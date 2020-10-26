bec59eab7f495ffb9d28f7e5ed1968b1663b8e4d
Drew Town <drew.town+github@gmail.com>
2020-10-26 13:54:38 -0600

    Add volunteer page

--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -1,5 +1,129 @@
 <template>
-  <div>
-    Volunteer
+  <div class="bg-white">
+    <div class="bg-green-600">
+      <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-screen-xl">
+        <div class="px-0 sm:px-4 lg:px-0 lg:flex lg:justify-between lg:items-center">
+          <div class="max-w-xl">
+            <h2 class="text-4xl leading-10 font-extrabold text-white sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+              Volunteer
+            </h2>
+            <p class="mt-5 text-xl leading-7 text-green-100">
+              Looking for a great workout? Meet wonderful people, get dirty, get fit and improve Ute Valley Park – all at the same time!
+            </p>
+          </div>
+          <div class="mt-10 w-full max-w-xs lg:mt-0">
+            <img src="~/assets/volunteer-2.jpg" class="rounded-lg shadow">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-gradient-to-b from-green-50 via-white to-white">
+      <!-- Pricing section with single price and feature list -->
+      <div class="max-w-screen-xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div class="pb-16 xl:flex xl:items-center xl:justify-between">
+          <div>
+            <h2 class="text-4xl leading-10 font-extrabold sm:text-5xl sm:leading-none sm:tracking-tight">
+              <span class="text-green-900">Learn More </span>
+            </h2>
+            <p class="mt-4 text-lg leading-7 text-cool-gray-500">
+              Meet your trail crew leader at the Pinon Valley Park (west) entrance to Ute Valley Park.
+              The meeting location can change, so please check the FUVP <a href="https://www.facebook.com/FriendsOfUteValleyPark/" class="text-green-600 font-semibold no-underline">Facebook</a> page for updates.
+              Any cancellations due to weather will also be posted on <a href="https://www.facebook.com/FriendsOfUteValleyPark/" class="text-green-600 font-semibold no-underline">Facebook</a>.
+            </p>
+          </div>
+        </div>
+        <div class="border-t border-cool-gray-200 pt-16 xl:grid xl:grid-cols-3 xl:col-gap-8">
+          <div>
+            <img src="~/assets/volunteer-3.jpg" class="object-cover rounded-lg shadow">
+          </div>
+          <div class="mt-4 sm:mt-8 md:mt-10 md:grid md:grid-cols-2 md:col-gap-8 xl:mt-0 xl:col-span-2">
+            <ul class="divide-y divide-cool-gray-200">
+              <li class="py-4 flex space-x-3 md:py-0 md:pb-4">
+                <!-- Heroicon name: check -->
+                <svg
+                  class="flex-shrink-0 h-6 w-6 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span class="text-base leading-6 text-cool-gray-500">Volunteer days: the 1st and 3rd Wednesday of every month, 5:30 – 7:30 p.m. April through September.</span>
+              </li>
+              <li class="py-4 flex space-x-3">
+                <!-- Heroicon name: check -->
+                <svg
+                  class="flex-shrink-0 h-6 w-6 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span class="text-base leading-6 text-cool-gray-500">Crew leaders schedule additional volunteer days. Check the FUVP <a href="https://www.facebook.com/FriendsOfUteValleyPark/" class="text-green-600 font-semibold no-underline">Facebook</a> page for volunteer opportunities, meeting locations and updates.</span>
+              </li>
+            </ul>
+            <ul class="border-t border-cool-gray-200 divide-y divide-cool-gray-200 md:border-t-0">
+              <li class="py-4 flex space-x-3 md:border-t-0 md:py-0 md:pb-4">
+                <!-- Heroicon name: check -->
+                <svg
+                  class="flex-shrink-0 h-6 w-6 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span class="text-base leading-6 text-cool-gray-500">Volunteer and get a free water bottle and T-shirt! After 4 hours of volunteer work you’ll receive a Friends of Ute Valley Park logo t-shirt and water bottle. <span class="text-grey-600 text-sm">Limit 1 water bottle and t-shirt per volunteer. </span></span>
+              </li>
+              <li class="py-4 flex space-x-3">
+                <!-- Heroicon name: check -->
+                <svg
+                  class="flex-shrink-0 h-6 w-6 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span class="text-base leading-6 text-cool-gray-500">All tools provided</span>
+              </li>
+              <li class="py-4 flex space-x-3">
+                <!-- Heroicon name: check -->
+                <svg
+                  class="flex-shrink-0 h-6 w-6 text-green-500"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <span class="text-base leading-6 text-cool-gray-500">Please wear closed toed shoes</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <u-contact default-option="Volunteer">
+      <template #title>
+        Contact us for more information!
+      </template>
+      <template #subtitle>
+        Help make Ute Valley Park better by volunteering.
+      </template>
+    </u-contact>
   </div>
 </template>
