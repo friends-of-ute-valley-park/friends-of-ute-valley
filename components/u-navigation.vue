f6b63db287ab55c6ce29e503340fa7a91a3167a5
Drew Town <drew.town+github@gmail.com>
2020-11-20 15:48:52 -0700

    Close mobile menu when navigation occurs

--- components/u-navigation.vue
+++ components/u-navigation.vue
@@ -57,6 +57,7 @@
           class="block pl-3 pr-4 py-2 border-l-4 text-base font-medium focus:outline-none transition duration-150 ease-in-out"
           :class="{'border-green-500 text-green-700 bg-green-50  focus:text-green-800 focus:bg-green-100 focus:border-green-700': $route.path === menuItem.route,
                    'border-transparent text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300': $route.path !== menuItem.route}"
+          @click.native="mobileMenuOpen = false"
         >
           {{ menuItem.title }}
         </nuxt-link>
