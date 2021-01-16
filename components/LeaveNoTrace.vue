22982b59fad85d4a8a507867881d26ccd06d4e60
Drew Town <drew.town+github@gmail.com>
2021-01-16 10:07:11 -0700

    Add no fire reminder

--- components/LeaveNoTrace.vue
+++ components/LeaveNoTrace.vue
@@ -22,6 +22,18 @@
           <p class="mt-3 max-w-3xl text-lg text-gray-50">
             Ute Valley Park proudly supports The 7 Principles of Leave No Trace. Please help keep the park safe and clean by following the principles.
           </p>
+          <p class="mt-3 max-w-3xl text-xl text-gray-50">
+            <svg
+              class="w-6 h-6 mb-1 text-gray-100 inline-block"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+            </svg>
+            Fires of any kind are <span class="font-extrabold text-green-50 uppercase">never</span> allowed in Ute Valley Park!
+          </p>
           <div class="mt-8 sm:flex">
             <div class="rounded-md shadow mx-auto bg-white p-8 w-64 focus-within:ring-2 focus-within:ring-green-100">
               <a href="https://www.lnt.org">
