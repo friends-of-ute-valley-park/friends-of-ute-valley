5fa95fed824b47f81cef1c9e18d1eed7592b4646
Drew Town <drew.town+github@gmail.com>
2021-04-03 07:55:37 -0600

    Move volunteer meeting location info to computed value

--- components/VolunteerItem.vue
+++ components/VolunteerItem.vue
@@ -20,7 +20,6 @@
         <div class="mt-2 sm:flex sm:justify-between">
           <div class="sm:flex">
             <a class="mt-2 flex items-center text-sm text-green-700 sm:mt-0 sm:ml-6 underline" :href="directionsLink">
-              <!-- Heroicon name: solid/location-marker -->
               <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
               </svg>
@@ -28,7 +27,6 @@
             </a>
           </div>
           <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-            <!-- Heroicon name: solid/calendar -->
             <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
             </svg>
@@ -52,12 +50,6 @@ export default {
       required: true
     }
   },
-  data () {
-    return {
-      meetingLocation: '',
-      directionsLink: ''
-    }
-  },
   computed: {
     volunteerDate () {
       return new Date(this.volunteerEvent.date)
@@ -67,17 +59,23 @@ export default {
         return new Intl.DateTimeFormat('en-US').format(this.volunteerDate)
       }
       return null
-    }
-  },
-  mounted () {
-    const predefinedLocation = this.volunteerEvent.meetingLocation.predefinedLocation
-    if (predefinedLocation !== -1) {
-      const location = this.meetingLocations.find(loc => loc.id === predefinedLocation)
-      this.meetingLocation = location.name
-      this.directionsLink = location.directionsLink
-    } else {
-      this.meetingLocation = this.volunteerEvent.meetingLocation.alternativeLocation
-      this.directionsLink = this.volunteerEvent.meetingLocation.alternativeLocationDirectionsLink
+    },
+    predefinedLocation () {
+      return this.volunteerEvent.meetingLocation.predefinedLocation
+    },
+    meetingLocation () {
+      if (this.predefinedLocation !== -1) {
+        const location = this.meetingLocations.find(loc => loc.id === this.predefinedLocation)
+        return location.name
+      }
+      return this.volunteerEvent.meetingLocation.alternativeLocation
+    },
+    directionsLink () {
+      if (this.predefinedLocation !== -1) {
+        const location = this.meetingLocations.find(loc => loc.id === this.predefinedLocation)
+        return location.directionsLink
+      }
+      return this.volunteerEvent.meetingLocation.alternativeLocationDirectionsLink
     }
   }
 }
