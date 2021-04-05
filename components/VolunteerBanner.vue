53b2b10d2b2b7ec3ed66219d0e78d6e0b24a5a09
Drew Town <drew.town+github@gmail.com>
2021-04-05 07:50:52 -0600

    Lighten and round volunteer banner

--- components/VolunteerBanner.vue
+++ components/VolunteerBanner.vue
@@ -1,28 +1,30 @@
 <template>
-  <div v-if="isVisible" class="bg-teal-700">
+  <div v-if="isVisible">
     <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-      <div class="flex items-center justify-between flex-wrap">
-        <div class="flex-1 flex items-center">
-          <span class="flex p-2 rounded-lg bg-teal-800">
-            <svg
-              class="h-6 w-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
-            </svg>
-          </span>
-          <p class="ml-3 font-medium text-white truncate">
-            Upcoming volunteer <span class="font-semibold">{{ eventDates }}!</span>
-            <span class="block sm:ml-2 sm:inline-block">
-              <nuxt-link to="/volunteer" class="text-white font-bold underline">
-                Learn More <span aria-hidden="true">&rarr;</span>
-              </nuxt-link>
+      <div class="p-2 rounded-lg bg-teal-600 shadow-lg sm:p-3">
+        <div class="flex items-center justify-between flex-wrap">
+          <div class="flex-1 flex items-center">
+            <span class="flex p-2 rounded-lg bg-teal-700">
+              <svg
+                class="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+              </svg>
             </span>
-          </p>
+            <p class="ml-3 font-medium text-white truncate">
+              Upcoming volunteer <span class="font-semibold">{{ eventDates }}!</span>
+              <span class="block sm:ml-2 sm:inline-block">
+                <nuxt-link to="/volunteer" class="text-white font-bold underline">
+                  Learn More <span aria-hidden="true">&rarr;</span>
+                </nuxt-link>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
