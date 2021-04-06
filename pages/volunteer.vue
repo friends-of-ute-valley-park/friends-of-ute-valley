c948fb7d4df82ab77a432655795d4a2b353e5148
Drew Town <drew.town+github@gmail.com>
2021-04-06 12:33:29 -0600

    slightly darken volunteer banner

--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white">
     <div class="max-w-7xl mx-auto">
-      <div class="bg-gradient-to-r from-green-400 to-teal-500 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+      <div class="bg-gradient-to-r from-green-500 to-teal-500 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
         <div class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
           <div class="lg:self-center">
             <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
