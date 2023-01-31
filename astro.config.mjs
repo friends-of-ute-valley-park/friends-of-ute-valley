143c8c42e37e19fe93bb8b7069a097b7c0defad5
Drew Town <drew.town+github@gmail.com>
2023-01-31 14:04:56 -0700

    add a workaround for the image optimization picking up vue src

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,21 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
-  integrations: [tailwind(), vue(), image()]
+  integrations: [
+    tailwind(),
+    vue({
+      template: {
+        // workaround for https://github.com/withastro/astro/issues/6071
+        transformAssetUrls: false,
+      },
+    }),
+    image(),
+  ],
 });
