c478fbd9b09de7c9ff561c1b65bdd14cb6cda969
Drew Town <drew.town+github@gmail.com>
2023-11-10 14:03:50 -0700

    Update classes and fix alt tag

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,10 +1,10 @@
 <template>
   <Disclosure v-slot="{ open }" as="nav" class="bg-white shadow">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex justify-between h-16">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 justify-between">
         <div class="flex">
-          <div class="flex-shrink-0 flex items-center">
-            <img class="block h-14 w-14" src="/images/logo-small.jpg" alt="Workflow" />
+          <div class="flex flex-shrink-0 items-center">
+            <img class="block h-14 w-14" src="/images/logo-small.jpg" alt="Friends of Ute Valley Park" />
           </div>
           <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
             <!-- Current: "border-green-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
@@ -15,7 +15,7 @@
               :aria-current="item.current ? 'page' : undefined"
               :class="[
                 item.current ? 'border-green-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium',
+                'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium',
               ]"
               >{{ item.name }}</a
             >
@@ -27,7 +27,7 @@
         <div class="-mr-2 flex items-center sm:hidden">
           <!-- Mobile menu button -->
           <DisclosureButton
-            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
+            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
             <span class="sr-only">Open main menu</span>
             <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
@@ -37,7 +37,7 @@
     </div>
 
     <DisclosurePanel class="sm:hidden">
-      <div class="pt-2 pb-3 space-y-1">
+      <div class="space-y-1 pb-3 pt-2">
         <!-- Current: "bg-green-50 border-green-500 text-green-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
         <DisclosureButton
           v-for="item in navigation"
@@ -45,8 +45,8 @@
           as="a"
           :href="item.href"
           :class="[
-            item.current ? 'bg-green-50 border-green-500 text-green-700' : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700',
-            'block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6',
+            item.current ? 'border-green-500 bg-green-50 text-green-700' : 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700',
+            'block border-l-4 py-2 pl-3 pr-4 text-base font-medium sm:pl-5 sm:pr-6',
           ]"
           >{{ item.name }}</DisclosureButton
         >
@@ -63,7 +63,7 @@
           </span>
         </div>
       </div>
-      <div class="-ml-4 my-4 flex justify-center pb-6">
+      <div class="my-4 -ml-4 flex justify-center pb-6">
         <slot name="social-links" />
       </div>
     </DisclosurePanel>
