be012482694c28fe5ab0ae0c1a0255756caaa084
Drew Town <drew.town+github@gmail.com>
2024-11-25 13:26:22 -0700

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './remark-reading-time.mjs';
 import vue from '@astrojs/vue';
 import Components from 'unplugin-vue-components/vite';
 import Icons from 'unplugin-icons/vite';
@@ -14,9 +13,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.friendsofutevalleypark.com',
   //trailingSlash: 'always',
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
   integrations: [
     tailwind(),
     vue({
