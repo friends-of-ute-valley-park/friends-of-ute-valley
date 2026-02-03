2c8d751db0569cf39e1d7412a3592221d1bc3b9f
Drew Town <drew.town+github@gmail.com>
2026-02-03 14:17:12 -0700

    tailwind v4 changes

--- src/components/VolunteerItem.vue
+++ src/components/VolunteerItem.vue
@@ -6,7 +6,7 @@
     <div class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
       <p>
         <time :datetime="formattedDate + ' ' + volunteerEvent.data.time">{{ formattedDate }} {{ volunteerEvent.data.time
-          }}</time>
+        }}</time>
       </p>
       <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
         <circle cx="1" cy="1" r="1" />
