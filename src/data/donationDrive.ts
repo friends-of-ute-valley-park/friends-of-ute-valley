a9dab90db9302b7f123e4d5e00b5b53b5608e16e
Drew Town <drew.town+github@gmail.com>
2026-06-24 19:09:32 -0600

    Centralize donation drive info

---
+++ src/data/donationDrive.ts
@@ -0,0 +1,22 @@
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 0,
+});
+
+const raised = 498;
+const goal = 2000;
+const progressPercent = Math.min((raised / goal) * 100, 100);
+
+export const donationDrive = {
+  name: 'Independence Kickoff Fund',
+  matchRatioLabel: '2-to-1',
+  deadlineLabel: 'August 31, 2026',
+  raised,
+  goal,
+  raisedFormatted: currencyFormatter.format(raised),
+  goalFormatted: currencyFormatter.format(goal),
+  progressPercent,
+  progressStyle: `--match-progress: ${progressPercent.toFixed(2)}%`,
+  challengeCapFormatted: currencyFormatter.format(goal),
+};
