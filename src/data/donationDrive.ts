11b4e1e22c56091a0d2e7fedd1b505bb1ff100d1
Drew Town <drew.town+github@gmail.com>
2026-07-02 13:51:38 -0600

    Update donationDrive.ts

--- src/data/donationDrive.ts
+++ src/data/donationDrive.ts
@@ -4,7 +4,7 @@ const currencyFormatter = new Intl.NumberFormat('en-US', {
   maximumFractionDigits: 0,
 });
 
-const raised = 1383;
+const raised = 1433;
 const goal = 2000;
 const progressPercent = Math.min((raised / goal) * 100, 100);
 
